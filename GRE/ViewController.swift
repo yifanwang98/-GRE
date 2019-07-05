808230bace3dce412456af686ce7bf08e4f96aff
Yifan Wang <yifanwang07@163.com>
2019-07-04 20:05:59 -0400

    Initial Commit

---
+++ GRE/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GRE
+//
+//  Created by Yifan Wang on 2019/7/4.
+//  Copyright © 2019 Yifan Wang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

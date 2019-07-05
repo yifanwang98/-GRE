a984b3009deded02aa3bec2b8f0f72e686b064c9
Yifan Wang <yifanwang07@163.com>
2019-07-04 21:14:45 -0400

    .

---
+++ GRE/StudyViewController.swift
@@ -0,0 +1,29 @@
+//
+//  StudyViewController.swift
+//  GRE
+//
+//  Created by Yifan Wang on 2019/7/4.
+//  Copyright © 2019 Yifan Wang. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class StudyViewController: UIViewController {
+    
+    @IBOutlet weak var wordLabel: UIView!
+    @IBOutlet weak var meaningLabel: UILabel!
+    @IBOutlet weak var explainationLabel: UILabel!
+    
+    @IBOutlet weak var rememberedBTN: UIButton!
+    @IBOutlet weak var option1: UIButton!
+    @IBOutlet weak var option2: UIButton!
+    @IBOutlet weak var option3: UIButton!
+    @IBOutlet weak var option4: UIButton!
+    @IBOutlet weak var dontKnow: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
